0ed05e736a3f3486657378679c4344b29ef031e1
Courtney Adams <cadams8238@gmail.com>
2019-06-01 22:45:17 -0700

    created basic db models, possibly need foreign key restraints for location related data

--- temperature.go
+++ temperature.go
@@ -5,24 +5,16 @@ import (
 	"time"
 )
 
-type Temp struct {
-	ID        int64 `gorm:"primary_key"`
-	TimeStamp time.Time
-	Value     float64
-	Type      string
-	Location  string
-}
-
 // add new temp to db
 func newTemp(temp float64, measurementType string, location string) {
-	db.AutoMigrate(&Temp{})
-	newTemp := Temp{TimeStamp: time.Now(), Value: temp, Location: location}
+	db.AutoMigrate(&soilTemp{})
+	newTemp := soilTemp{TimeStamp: time.Now(), Value: temp, Location: location}
 	db.Create(&newTemp)
 }
 
 // get
 func getTemps(dateTimeStart time.Time, dataTimeEnd time.Time) {
-	lastTemp := Temp{}
+	lastTemp := soilTemp{}
 	db.Last(&lastTemp)
 	log.Printf(lastTemp.Location)
 }
