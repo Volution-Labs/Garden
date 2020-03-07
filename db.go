4f481e279a68520564e33c2a7490e67fd08dcc7c
Courtney Adams <cadams8238@gmail.com>
2020-03-07 15:36:19 -0800

    refactor initDB() to create a db file if it doesn't exist

--- db.go
+++ db.go
@@ -1,17 +1,45 @@
 package main
 
 import (
+	"os"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 var db *gorm.DB
 
-func initDB() {
-	var err error
-	db, err = gorm.Open("sqlite3", "./data/sqlite3.db")
-	if err != nil {
-		panic("Error opening db")
+func initDB() error {
+	dataFilepath := "./data/sqlite3.db"
+	if err := checkAndCreateFileIfNotExists(dataFilepath); err != nil {
+		return err
 	}
+
+	var err error
+	db, err = gorm.Open("sqlite3", dataFilepath)
+	if err != nil {
+		return err
+	}
+
 	db.LogMode(true)
+	return nil
+}
+
+func checkAndCreateFileIfNotExists(filename string) error {
+	if exists := doesFileExist(filename); !exists {
+		if _, err := os.Create(filename); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// doesFileExists checks if a file exists and is not a
+// directory before we try using it to prevent further errors.
+func doesFileExist(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
 }
